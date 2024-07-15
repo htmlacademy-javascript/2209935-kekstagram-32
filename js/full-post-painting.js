ca929ddc1042c63dd9cc9d4a631f7dd88c22e552
Sergey Buy <154270878+SerIBuy@users.noreply.github.com>
2024-07-15 16:10:55 +0300

    Сделано ДЗ Открывается и закрывается без рефакторинга

--- js/full-post-painting.js
+++ js/full-post-painting.js
@@ -6,17 +6,41 @@ const bigPicture = document.querySelector('.big-picture');
 const bigPictureImage = bigPicture.querySelector('.big-picture__img img');
 const likesCount = bigPicture.querySelector('.likes-count');
 const bigPictureCloseButton = bigPicture.querySelector('.big-picture__cancel');
+const commentCount = bigPicture.querySelector('.social__comment-count');
+const commentsTotalCount = commentCount.querySelector('.social__comment-total-count');
+const commentsShownCount = commentCount.querySelector('.social__comment-shown-count');
+const postDescription = bigPicture.querySelector('.social__caption');
+const commentTemplate = document.querySelector('#comment').content.querySelector('.social__comment');
+const commentsList = bigPicture.querySelector('.social__comments');
+const commentsLoaderButton = bigPicture.querySelector('.social__comments-loader');
 
-const addThumbnailClickHandler = (thumbnail, bigPhoto, likes) => { // функция вешает обработчики на миниатюры
+const addThumbnailClickHandler = (thumbnail, bigPhoto, likes, comments, description) => { // функция отрисовки поста при клике
   thumbnail.addEventListener('click', () => {
+    const commentFragment = document.createDocumentFragment();
     bigPictureImage.src = bigPhoto;
+    postDescription.textContent = description;
     likesCount.textContent = likes;
+    commentsTotalCount.textContent = comments.length;
+    commentsShownCount.textContent = comments.length;
+
+    comments.forEach(({avatar, message, name}) => {
+      const comment = commentTemplate.cloneNode(true);
+      const commentPicture = comment.querySelector('.social__picture');
+      commentPicture.src = avatar;
+      commentPicture.alt = name;
+      comment.querySelector('.social__text').textContent = message;
+      commentFragment.appendChild(comment);
+    });
+    commentsList.appendChild(commentFragment);
+    commentCount.classList.add('hidden');
+    commentsLoaderButton.classList.add('hidden');
     bigPicture.classList.remove('hidden');
+    document.body.classList.add('modal-open');
   });
 };
 
 for (let i = 0; i < thumbnails.length; i++) { // в цикле навешиваем обработчики
-  addThumbnailClickHandler(thumbnails[i], posts[i].url, posts[i].likes);
+  addThumbnailClickHandler(thumbnails[i], posts[i].url, posts[i].likes, posts[i].comments, posts[i].description);
 }
 
 bigPictureCloseButton.addEventListener('click', (evt) => { // обрабочик закрытия окна по кнопке
