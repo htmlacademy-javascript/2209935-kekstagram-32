9471e96f0a4060c71607d26ec1330894ef0551b4
Sergey Buy <154270878+SerIBuy@users.noreply.github.com>
2024-07-16 11:24:45 +0300

    ДЗ сделано но, кажется, нужны доработки

--- js/full-post-painting.js
+++ js/full-post-painting.js
@@ -14,8 +14,27 @@ const commentTemplate = document.querySelector('#comment').content.querySelector
 const commentsList = bigPicture.querySelector('.social__comments');
 const commentsLoaderButton = bigPicture.querySelector('.social__comments-loader');
 
+const onClosePostKeydown = (evt) => { // функция обработки закрытия поста клавишей esc
+  if (isPressedKeyEscape(evt)) {
+    evt.preventDefault();
+    closePost();
+  }
+};
+
+const onClosePostClick = (evt) => { // функция обработки закрытия поста кнопкой крестиком
+  evt.preventDefault();
+  closePost();
+};
+
+function closePost () { // функция закрытия окна поста
+  bigPicture.classList.add('hidden');
+  document.removeEventListener('keydown', onClosePostKeydown);
+  bigPictureCloseButton.removeEventListener('click', onClosePostClick);
+}
+
 const addThumbnailClickHandler = (thumbnail, bigPhoto, likes, comments, description) => { // функция отрисовки поста при клике
   thumbnail.addEventListener('click', () => {
+
     const commentFragment = document.createDocumentFragment();
     bigPictureImage.src = bigPhoto;
     postDescription.textContent = description;
@@ -31,26 +50,19 @@ const addThumbnailClickHandler = (thumbnail, bigPhoto, likes, comments, descript
       comment.querySelector('.social__text').textContent = message;
       commentFragment.appendChild(comment);
     });
+
     commentsList.appendChild(commentFragment);
     commentCount.classList.add('hidden');
     commentsLoaderButton.classList.add('hidden');
     bigPicture.classList.remove('hidden');
     document.body.classList.add('modal-open');
+
+    document.addEventListener('keydown', onClosePostKeydown); // обработчик закрытия окна по клавише esc
+
+    bigPictureCloseButton.addEventListener('click', onClosePostClick); // обрабочик закрытия окна по кнопке;
   });
 };
 
 for (let i = 0; i < thumbnails.length; i++) { // в цикле навешиваем обработчики
   addThumbnailClickHandler(thumbnails[i], posts[i].url, posts[i].likes, posts[i].comments, posts[i].description);
 }
-
-bigPictureCloseButton.addEventListener('click', (evt) => { // обрабочик закрытия окна по кнопке
-  evt.preventDefault();
-  bigPicture.classList.add('hidden');
-});
-
-document.addEventListener('keydown', (evt) => { // обработчик закрытия окна по клавише esc
-  if (isPressedKeyEscape(evt)) {
-    evt.preventDefault();
-    bigPicture.classList.add('hidden');
-  }
-});
