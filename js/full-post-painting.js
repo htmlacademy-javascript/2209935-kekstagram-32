5c30bda17cbc4883338367afe0d93fadbf49f014
Sergey Buy <154270878+SerIBuy@users.noreply.github.com>
2024-07-17 13:45:21 +0300

    Реализован показ помментариев по кнопке Загрузить еще

--- js/full-post-painting.js
+++ js/full-post-painting.js
@@ -1,6 +1,9 @@
 import { isPressedKeyEscape } from './utils.js'; // импортируем функцию проверки нажата ли клавиша ESC
 import { posts } from './thumbnails-painting.js';
 
+const SHOWN_COMMENTS_PERIOD = 5;
+let shownCommentsCount = 0;
+
 // записываем в переменные необходимые узлы DOM
 const bigPicture = document.querySelector('.big-picture');
 const bigPictureImage = bigPicture.querySelector('.big-picture__img img');
@@ -34,22 +37,40 @@ function closePost () {
   document.body.classList.remove('modal-open');
   document.removeEventListener('keydown', onPostCloseButtonKeydown);
   bigPictureCloseButton.removeEventListener('click', onPostCloseButtonClick);
+  shownCommentsCount = 0;
   commentsList.innerHTML = '';
-
+  commentsLoaderButton.classList.remove('hidden');
 }
 
 // функция отрисовывает комментарии
 const paintComments = (comments) => {
   const commentFragment = document.createDocumentFragment();
-  comments.forEach(({avatar, message, name}) => {
-    const comment = commentTemplate.cloneNode(true);
-    const commentPicture = comment.querySelector('.social__picture');
-    commentPicture.src = avatar;
-    commentPicture.alt = name;
-    comment.querySelector('.social__text').textContent = message;
-    commentFragment.appendChild(comment);
-  });
-  return commentFragment;
+  const workVersionComments = structuredClone(comments);
+  let currentCommentsCount = 0;
+  return () => {
+    if (workVersionComments.length < SHOWN_COMMENTS_PERIOD) {
+      currentCommentsCount = workVersionComments.length;
+    } else {
+      currentCommentsCount = 5;
+    }
+    shownCommentsCount += currentCommentsCount;
+    commentsShownCount.textContent = shownCommentsCount;
+    const partComments = workVersionComments.splice(0, currentCommentsCount);
+    partComments.forEach(({avatar, message, name}) => {
+      const comment = commentTemplate.cloneNode(true);
+      const commentPicture = comment.querySelector('.social__picture');
+      commentPicture.src = avatar;
+      commentPicture.alt = name;
+
+      if (workVersionComments.length === 0) {
+        commentsLoaderButton.classList.add('hidden');
+      }
+
+      comment.querySelector('.social__text').textContent = message;
+      commentFragment.appendChild(comment);
+    });
+    return commentFragment;
+  };
 };
 
 // функция отрисовки поста при клике на миниатюре
@@ -61,13 +82,20 @@ const onThumbnailClick = (id) => {
   postDescription.textContent = description;
   likesCount.textContent = likes;
   commentsTotalCount.textContent = comments.length;
-  commentsShownCount.textContent = comments.length;
 
   const paintedComments = paintComments(comments);
-  commentsList.appendChild(paintedComments);
+  let paintedCommentsPart = paintedComments();
+  commentsList.appendChild(paintedCommentsPart);
+
+  commentsShownCount.textContent = shownCommentsCount;
 
   bigPicture.classList.remove('hidden');
   document.body.classList.add('modal-open');
+
+  commentsLoaderButton.addEventListener('click', () => {
+    paintedCommentsPart = paintedComments();
+    commentsList.appendChild(paintedCommentsPart);
+  });
 
   document.addEventListener('keydown', onPostCloseButtonKeydown); // обработчик закрытия окна по клавише esc
 
