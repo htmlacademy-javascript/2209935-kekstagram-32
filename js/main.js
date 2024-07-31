e15d7a62e2535218417217cb4e4d037d8df339fe
Sergey Buy <154270878+SerIBuy@users.noreply.github.com>
2024-07-31 22:13:26 +0300

    реализовал фильтр случайные

--- js/main.js
+++ js/main.js
@@ -6,8 +6,42 @@ import './user-form.js';
 import './user-form-change-size-image.js';
 import './image-effects.js';
 
+const filterContainer = document.querySelector('.img-filters');
+filterContainer.classList.remove('img-filters--inactive');
+
+const onFilterClick = (cb) => {
+  filterContainer.addEventListener('click', (evt) => {
+    const target = evt.target.closest('.img-filters__button');
+    console.log(target);
+    if (target) {
+      if (target.getAttribute('id') === 'filter-random') {
+        const pictures = document.querySelectorAll('.picture');
+        for (let i = 0; i < pictures.length; i++) {
+          pictures[i].remove();
+        }
+        const posts = [2, 5, 6];
+        console.log('есть');
+        cb(posts);
+      } else if (target.getAttribute('id') === 'filter-discussed') {
+        const pictures = document.querySelectorAll('.picture');
+        for (let i = 0; i < pictures.length; i++) {
+          pictures[i].remove();
+        }
+        cb();
+      }
+    }
+  });
+};
+
 getData()
   .then((posts) => {
     paintPosts(posts);
+    onFilterClick((idPosts) => {
+      const filteredPosts = [];
+      for (let i = 0; i < idPosts.length; i++) {
+        filteredPosts[i] = posts[idPosts[i]];
+      }
+      paintPosts(filteredPosts);
+    });
   });
 
